c005c3a71e6d83c018540de23a00e020fd134748
gh6987 <61199814+gh6987@users.noreply.github.com>
2020-12-28 13:35:54 +0100

    path added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
+const path = require('path');
+
 const messages = require("./db/messages");
 const users = require("./db/users");
 
